990a75b3807528bcb3dd1e78affb01b7ddebae97
itkmm <khammam.task@gmail>
2024-01-16 17:32:41 +0530

    content_v1

---
+++ 4.Identifiers/index.js
@@ -0,0 +1,6 @@
+//To be able to proceed with the manipulation of the data assigned to the variables,
+//it is mandatory for a programming language to know the type of value or
+//the type of data that the variable holds.
+//That is because the operations or manipulations 
+//that must be applied on a variable will be specific to the type of data that a variable will hold. 
+
