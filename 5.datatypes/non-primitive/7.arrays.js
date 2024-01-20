7a19ba393e05d270660c2a83f824995ff473ec76
itkmm <khammam.task@gmail.com>
2024-01-20 11:14:44 +0530

    notes update

--- 5.datatypes/non-primitive/7.arrays.js
+++ 5.datatypes/non-primitive/7.arrays.js
@@ -1,7 +1,7 @@
 //The Array is a special data structure that is used to store an ordered collection, 
 //which cannot be achieved using the objects.  
 //There are two ways of creating an array: 
-let dummyArr = new Array();     
+let dummyArr1 = new Array();     
 //OR 
 let dummyArr = []; //Note:A single array can hold multiple values of different data types. 
 
---
+++ 7.statements&Expressions/index.js
@@ -0,0 +1,19 @@
+//Statements are instructions in JavaScript that have to be executed by a web browser. 
+//JavaScript code is made up of a sequence of statements and is executed 
+//in the same order as they are written.
+
+//A Variable declaration is the simplest example of a JavaScript statement.
+
+//Example
+var firstName = "Newton" ; 
+
+//Other types of JavaScript statements include conditions/decision making, loops, etc.
+
+//White (blank) spaces in statements are ignored.
+
+//It is optional to end each JavaScript statement with a semicolon. 
+//But it is highly recommended to use it as it avoids possible misinterpretation 
+//of the end of the statements by JavaScript engine.
+
+//While writing client-logic in JavaScript, variables and operators are often combined to do computations. 
+//This is achieved by writing expressions. 
